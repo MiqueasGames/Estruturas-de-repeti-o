ecdea1197438187d7a9044b2f42072b5fc1a3d74
Migs <omiguelantonio190@gmail.com>
2025-08-18 22:54:39 -0300

    impar

---
+++ impar/src/impar/impar.java
@@ -0,0 +1,25 @@
+package impar;
+
+import java.util.Scanner;
+
+public class impar {
+	public static void main(String[]args) {
+		Scanner en = new Scanner(System.in);
+		
+		for(int i = 0 ; i < 51 ; i++) {
+			
+			if(i % 2 != 0) {
+			
+			
+			System.out.println(+i);
+				
+			}
+		}
+		
+		en.close();
+	
+	}
+
+	
+	
+}
