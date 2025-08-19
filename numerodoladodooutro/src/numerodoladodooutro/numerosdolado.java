df1f9553c67324a05467fcde84c7e9a392030766
Migs <omiguelantonio190@gmail.com>
2025-08-18 22:58:42 -0300

    mostra um numero do lado do outro

---
+++ numerodoladodooutro/src/numerodoladodooutro/numerosdolado.java
@@ -0,0 +1,23 @@
+package numerodoladodooutro;
+
+import java.util.Scanner;
+
+public class numerosdolado {
+	public static void main(String[]args) {
+		Scanner en = new Scanner(System.in);
+		
+		for (int i = 1 ; i < 21; i++) {
+			
+			System.out.println(+i);
+			
+		}
+		
+		for (int i = 1 ; i < 21; i++) {
+			
+			System.out.print(+i+ ", ");
+		
+		}
+
+	}
+	
+}
